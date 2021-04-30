cf9918bc359e0b99493bed538e3ab3a42429a6cf
sysorion2017 <sysorion2017@gmail.com>
2021-04-29 21:32:06 -0500

    Calculo del resultado y IMC

--- lib/page/resultados.dart
+++ lib/page/resultados.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class ResultadoPage extends StatelessWidget {
   final int edad, peso, estatura;
   final bool genero;
+  String resultado = "", mensaje = "";
+  double valor = 0;
+  Color color = Colors.white;
 
   ResultadoPage(
       {required this.edad,
@@ -20,7 +23,8 @@ class ResultadoPage extends StatelessWidget {
     );
   }
 
-  Widget _body(BuildContext context) {    
+  Widget _body(BuildContext context) {
+    _calculo();
     return Column(
       children: [
         Container(
@@ -35,28 +39,30 @@ class ResultadoPage extends StatelessWidget {
           child: Container(
             margin: EdgeInsets.all(10),
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),              
+              borderRadius: BorderRadius.circular(15),
+              color: Colors.grey[800],
             ),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 Center(
                   child: Text(
-                    "Normal",
+                    "$resultado",
                     style: TextStyle(
                         fontSize: 20,
-                        fontWeight: FontWeight.bold),
+                        fontWeight: FontWeight.bold,
+                        color: color),
                   ),
                 ),
                 Center(
                   child: Text(
-                    "22.5",
+                    "$valor",
                     style: TextStyle(fontSize: 90, fontWeight: FontWeight.bold),
                   ),
                 ),
                 Center(
                   child: Text(
-                    "Tiene un peso corporal normal. ¡Buen trabajo!",
+                    "$mensaje",
                     style: TextStyle(fontSize: 20),
                     textAlign: TextAlign.center,
                   ),
@@ -76,7 +82,8 @@ class ResultadoPage extends StatelessWidget {
                   style: ButtonStyle(
                       backgroundColor:
                           MaterialStateProperty.all<Color>(Colors.pink)),
-                  onPressed: () {                    
+                  onPressed: () {
+                    Navigator.pop(context);
                   },
                   child: Text(
                     "Calcular",
@@ -93,4 +100,35 @@ class ResultadoPage extends StatelessWidget {
     );
   }
 
+  void _calculo() {
+    valor = estatura / 100;
+    valor *= valor;
+    valor = peso / valor;
+    valor = double.parse(valor.toStringAsFixed(1));
+    if (valor < 18.5) {
+      resultado = "BAJO PESO";
+      mensaje = "Su peso corporal esta bajo. ¡Cuidado!";
+      color = Colors.yellow;
+    } else if (valor < 24.9) {
+      resultado = "PESO NORMAL";
+      mensaje = "Su peso corporal esta normal. ¡Buen trabajo!";
+      color = Colors.green;
+    } else if (valor < 29.9) {
+      resultado = "SOBREPESO";
+      mensaje = "Cuidado usted tiene sobrepeso. ¡Por favor tomar medidas!";
+      color = Colors.yellow;
+    } else if (valor < 34.5) {
+      resultado = "OBESIDAD GRADO I";
+      mensaje = "Usted Tiene obesidad nivel 1. ¡Ten cuidado, esto es una enfermedad!";
+      color = Colors.amber;
+    } else if (valor < 39.9) {
+      resultado = "OBESIDAD GRADO II";
+      mensaje = "Usted tiene obesidad nivel 2. Por favor cuidese, esto es grave!";
+      color = Colors.orange;
+    } else {
+      resultado = "OBESIDAD GRADO III";
+      mensaje = "Usted tiene obesidad nivel 3. Cuidado, alerta roja, su salud corre peligro!";
+      color = Colors.red;
+    }
+  }
 }
